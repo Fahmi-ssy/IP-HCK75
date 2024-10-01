76de4bbaa12672a4674eeb90e1d0dafabb8d1b5e
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-10-01 19:28:04 +0700

    models associate

---
+++ SERVER/models/inventory.js
@@ -0,0 +1,102 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Inventory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Inventory.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user'
+      })
+      Inventory.hasMany(models.Cart, {
+        foreignKey: 'product_id',
+        as: 'cart'
+      })
+    }
+  }
+  Inventory.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Title is required'
+        },
+        notNull: {
+          msg: 'Title is required'
+        }
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Price is required'
+        },
+        notNull: {
+          msg: 'Price is required'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Description is required'
+        },
+        notNull: {
+          msg: 'Description is required'
+        }
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Category is required'
+        },
+        notNull: {
+          msg: 'Category is required'
+        }
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Image is required'
+        },
+        notNull: {
+          msg: 'Image is required'
+        }
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Rating is required'
+        },
+        notNull: {
+          msg: 'Rating is required'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Inventory',
+  });
+  return Inventory;
+};
---
+++ SERVER/models/user.js
@@ -0,0 +1,72 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.Cart, {
+        foreignKey: 'user_id',
+        as: 'cart'
+      })
+      User.hasMany(models.Inventory)
+      
+
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Username is required'
+        },
+        notNull: {
+          msg: 'Username is required'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Email is required'
+        },
+        notNull: {
+          msg: 'Email is required'
+        },
+        isEmail: {
+          msg: 'Email is invalid'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Password is required'
+        },
+        notNull: {
+          msg: 'Password is required'
+        },
+        len: {
+          args: [8, 20],
+          msg: 'Password must be between 8 and 20 characters'
+        }
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
