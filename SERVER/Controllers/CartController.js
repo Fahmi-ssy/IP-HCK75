872267d64bddd7c056689db058c97cfea98c00d8
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-10-02 19:54:03 +0700

    CreateCartItem Succsess

--- SERVER/Controllers/CartController.js
+++ SERVER/Controllers/CartController.js
@@ -1,3 +1,4 @@
+const {Cart, Inventory} = require('../models');
 
 class CartController{
     static async createCartItem(req, res, next) {
@@ -5,6 +6,19 @@ class CartController{
             const { user_id, product_id, quantity, total_price } = req.body;
             const newCartItem = await Cart.create({ user_id, product_id, quantity, total_price });
             res.status(201).json(newCartItem);
+        } catch (error) {
+            console.error(error);
+            res.status(400).json({ error: error.message });
+        }
+    }
+    static async getCartItems(req, res, next) {
+        try {
+            const userId = req.params.userId;
+            const cartItems = await Cart.findAll({
+                where: { user_id: userId },
+                include: [Inventory] 
+            });
+            res.status(200).json(cartItems);
         } catch (error) {
             console.error(error);
             res.status(400).json({ error: error.message });
--- SERVER/routers/CartRouter.js
+++ SERVER/routers/CartRouter.js
@@ -2,8 +2,10 @@ const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
 const CartController = require("../Controllers/CartController");
 const CartRouter = require("express").Router();
+
 CartRouter.use(authentication);
 CartRouter.use(authorization);
-CartRouter.get("/", CartController.createCartItem);
+
+CartRouter.post("/", CartController.createCartItem);
 
 module.exports = CartRouter;
