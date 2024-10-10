8a63a41186d3c8af3f77dfa46c9356c1fef3d552
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-10-11 01:43:50 +0700

    Create and Update Function

--- client/router.jsx
+++ client/router.jsx
@@ -3,26 +3,27 @@ import Register from "./src/components/register";
 import Login from "./src/components/login";
 import MainPages from "./src/pages/MainPages";
 import CreateInventory from "./src/components/CreateInventory";
+import UpdateInventory from "./src/components/UpdateInventory";
 
 export const router = createBrowserRouter([
-    {
-        path: "/register",
-        element: <Register/>
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/",
-        element: <MainPages/>
-    },
-    {
-        path: "/create",
-        element: <CreateInventory/>
-    }
-
-
-    
-
-])
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <MainPages />,
+  },
+  {
+    path: "/create",
+    element: <CreateInventory />,
+  },
+  {
+    path: "/update/:id",
+    element: <UpdateInventory />,
+  },
+]);
